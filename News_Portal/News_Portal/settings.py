e76c8c9d7108ea5b4c9ba4ed40f9151f1afc00d3
NikolayBaskakov <bnn300702@yandex.ru>
2023-12-28 23:58:39 +0300

    Add CELERY params into settings.py

--- News_Portal/News_Portal/settings.py
+++ News_Portal/News_Portal/settings.py
@@ -163,3 +163,9 @@ MANAGERS = [
     ('Ivan', 'ivandrugmoy@gmail.com'),
     ('Baran', 'somebaran@mail.ru')
 ]
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
