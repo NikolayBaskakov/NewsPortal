63dbca1e32bdabd56dc667e1e380495424f16a20
NikolayBaskakov <bnn300702@yandex.ru>
2023-12-31 02:14:39 +0300

    m,

--- News_Portal/NewsPaper/signals.py
+++ News_Portal/NewsPaper/signals.py
@@ -5,5 +5,6 @@ from .models import Post, Subscriber, PostCategory
 from django.core.mail import EmailMultiAlternatives
 from .tasks import send_if_post_created
 @receiver(m2m_changed, sender=Post.category.through)
-def post_created(instance, **kwargs):
-    send_if_post_created.delay(instance_id=instance.id)
+def post_created(sender, instance, action, **kwargs):
+    if action == 'post_add' and instance.__class__.__name__ == 'Post':
+        send_if_post_created.delay(instance_id=instance.id)
