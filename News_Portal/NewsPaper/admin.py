83f38440743afb26c5990434ccaf3051b9a38d91
NikolayBaskakov <bnn300702@yandex.ru>
2024-01-15 13:17:16 +0300

    c

--- News_Portal/NewsPaper/admin.py
+++ News_Portal/NewsPaper/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+def update_rating(modeladmin, request, queryset):
+    queryset.update(rating=0)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'date', 'title', 'rating')
+    list_filter = ('date',)
+    search_fields = ('name', 'category__name')
+    actions = [update_rating]
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(PostCategory)
