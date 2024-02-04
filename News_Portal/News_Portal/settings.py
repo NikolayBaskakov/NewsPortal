fb29553acc8c2a57e58e8fda55db4bacc9d521ad
Nikolay Baskakov <bnn300702@yandex.com>
2024-02-04 21:18:55 +0300

    set

--- News_Portal/News_Portal/settings.py
+++ News_Portal/News_Portal/settings.py
@@ -53,6 +53,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -294,6 +295,7 @@ LOGGING = {
         },
     }
 }
+
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale')
 ]
